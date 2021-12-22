0aaf980c5c5ca4dd57a23794f797a7fe765e2215
justTakhir <tantoy@inbox.ru>
2021-12-22 21:52:24 +0300

    Delete invalid tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,58 +1,16 @@
-using System.Collections.Generic;
-using System.Data;
 using System.IO;
-using System.Net;
 using Newtonsoft.Json;
 using NUnit.Framework;
-using TournamentManager;
 using TournamentManager.Model;
 
 namespace Tests
 {
     public class Tests
     {
-        [Test]
-        public void Test1()
-        {
-            var tournamentManager = new TournamentManager.TournamentManager();
-            var player1 = new Player(0, "player1", 1, 23);
-            var player2 = new Player(1, "player2", 0, 32);
-            var players = new List<Player> {player1, player2};
-            var table = tournamentManager.CreateTableBySingleElimination(players, 8);
-            Assert.True(table.Groups.Count == 1);
-            Assert.True(table.Groups[0].Players[0] == player2);
-        }
-
-        [TestCase(7, 3)]
-        public void Test2(int n, int k)
-        {
-            var tournamentManager = new TournamentManager.TournamentManager();
-            var players = new List<Player>();
-            for (int i = 0; i < n; i++)
-            {
-                var player = new Player(i, $"player{i + 1}", 0, 0);
-                players.Add(player);
-            }
-
-            var table = tournamentManager.CreateTableByRoundRobin(players, k);
-            ;
-            var outputStr = JsonConvert.SerializeObject(table, Formatting.Indented);
-            File.WriteAllText(
-                $"TestData/Example_RR/RR_output{n}_{k}.json",
-                outputStr
-            );
-        }
-
-        /*[TestCase("example")]
-        [TestCase("example2")]
-        [TestCase("example3")]
-        [TestCase("example4")]
-        [TestCase("example5")]
-        [TestCase("example6")]*/
-        [TestCase("Example_DE/Test1")]
-        [TestCase("Example_DE/Test2")]
-        [TestCase("Example_DE/Test3")]
-        [TestCase("Example_DE/Test4")]
+        [TestCase("DoubleElimination/Test1")]
+        [TestCase("DoubleElimination/Test2")]
+        [TestCase("DoubleElimination/Test3")]
+        [TestCase("DoubleElimination/Test4")]
         public void Test12(string testName)
         {
             var inputSettingsStr = File.ReadAllText($"TestData/{testName}/input_settings.json");
@@ -72,11 +30,6 @@ namespace Tests
             }
 
             File.WriteAllText($"OutputData/{testName}/output.json", outputStr);
-            
-            // var forAssertStr = File.ReadAllText($"TestData/{testName}/output.json");
-            // var forAssert = JsonConvert.DeserializeObject<OutputSettings>(forAssertStr);
-            
-            // Assert.True(outputSettings == forAssert);
         }
     }
 }
